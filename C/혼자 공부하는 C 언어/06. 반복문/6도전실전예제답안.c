3d750c02df0030d56401688886053423b728d6b3
haeonlee <onleelife@gmail.com>
2022-06-13 22:39:01 +0900

    2022/06/13

--- C/혼자 공부하는 C 언어/06. 반복문/6도전실전예제답안.c
+++ C/혼자 공부하는 C 언어/06. 반복문/6도전실전예제답안.c
@@ -4,27 +4,27 @@ int main(void)
      int num;
      int i, j;
      int pn;
-     int cnt = 0;
+     int count = 0;
 
      printf("2이상의 정수를 입력하세요 : ");
      scanf("%d", &num);
 
      for(i = 2; i <= num; i++)
      {
-           pn = 1;
+           pn = 1;      // 이게 내가 안 한 거
            for(j = 2; j < i; j++)
            {
                 if((i % j) == 0)
                 {
-                      pn = 0;
+                      pn = 0; // 이게 내가 안 한 거
                       break;
                 }
            }
-           if(pn == 1)
+           if(pn == 1)  // 이렇게 빼 줘야 하고
            {
                 printf("%5d", i);
-                cnt++;
-                if((cnt % 5) == 0)
+                count++;
+                if((count % 5) == 0)
                 {
                       printf("\n");
                 }
